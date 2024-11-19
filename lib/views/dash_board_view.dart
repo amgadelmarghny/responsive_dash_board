9566ed4b3d11116cf5e3e48feff89b614aa421dc
Amgad Elmarghny <amgadsaad22@gmail.com>
2024-11-19 16:34:38 +0200

    create desktop for dash board view

---
+++ lib/views/dash_board_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custom_layout_builder.dart';
+import 'package:responsive_dash_board/widgets/desktop_layout.dart';
+
+class DashBoardView extends StatelessWidget {
+  const DashBoardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomLayoutBuilder(
+          mobileLayout: (context) => const SizedBox(),
+          tabletLayout: (context) => const SizedBox(),
+          desktopLayout: (context) => const DesktopLayout()),
+    );
+  }
+}
